c5c706e34e5f09cc1c4e16a8f131258af56a7e9c
roxxe3 <hamzafarissi@gmail.com>
2024-12-28 14:55:53 +0100

    Add payment form and thank you message to order completion flow

--- index.js
+++ index.js
@@ -3,15 +3,46 @@ import {menuArray} from "/data.js"
 
 const mainPage = document.getElementById("main")
 const orderDetails = document.getElementById("section-holder")
+const paymentForm = document.getElementById("payment-form")
+const completeBtn = document.getElementById("comple-btn")
+
 mainPage.innerHTML = generateMenuHtml()
 let orders = []
+let name =""
+
+paymentForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    
+    const payFormData = new FormData(paymentForm)
+    name = payFormData.get("name")
+    console.log(name)
+    paymentForm.style.display = "none"
+    document.getElementById("thanks-msg").textContent = `Thanks, ${name}! Your order is on its way!`
+    document.getElementById("thanks-you").style.display = "flex"
+    document.querySelector("footer").style.display = "none"
+
+})
+
+completeBtn.addEventListener("click", function(){
+    paymentForm.style.display = "flex"
+})
+
 
 document.addEventListener('click', function(e) {
     if (e.target.dataset.add) {
-        orders.push(menuArray.filter(function(item){
+        const itemToAdd = menuArray.find(function(item){
             return item.id == e.target.dataset.add
-        })[0])
+        })
+        if (!orders.some(orderItem => orderItem.id === itemToAdd.id)) {
+            orders.push(itemToAdd)
+        }
         console.log(orders)
+        handleaddorder()
+        }
+        else if (e.target.dataset.remove){
+        orders = orders.filter(function(item){
+            return item.id != e.target.dataset.remove
+        })
         handleaddorder()
     }
 });
@@ -41,20 +72,24 @@ return menuHtml
 
 }
 
-function handleaddorder(orderId) {
+function handleaddorder() {
     let orderHtml = ""
     orders.forEach(function(orderItem){
         orderHtml += 
         `
+        <ul>
         <div class="order-details">
             <div class="order-summary">
-                <h2>${orderItem.name}</h2>
-                <button  id="remove-btn">remove</button>
+                <li><h2>${orderItem.name}</h2><li>
+                <button  id="remove-btn" data-remove="${orderItem.id}">remove</button>
                 </div>
-            <p>$${orderItem.price}</p>
+            <li><p>$${orderItem.price}</p></li>
+            </ul>
         `
     })
-
+    document.getElementById("price").textContent = "$" + orders.reduce(function(total, currentTotal) {
+        return total + currentTotal.price
+    }, 0) 
     orderDetails.innerHTML = orderHtml
     document.querySelector("footer").style.display = "block"
 }
